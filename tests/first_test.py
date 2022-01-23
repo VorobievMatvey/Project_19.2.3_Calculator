499ac086a032a5b1aa1ccb8b8dddda9d0be76690
Matvey Vorobiev <matvey.vorobiev@mail.ru>
2022-01-23 15:59:06 +0300

    Upload

---
+++ tests/first_test.py
@@ -0,0 +1,19 @@
+import pytest
+from app.calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.multiply(self, 1, 2) == 3
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 3) == 6
+
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self, 8, 2) == 4
+
+    def test_substraction_calculate_correctly(self):
+        assert self.calc.subtraction(self, 7, 4) == 3
